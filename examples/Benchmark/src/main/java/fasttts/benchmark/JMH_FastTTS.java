63b0dc1129d748fb2985cedd358716cfc1356d89
Andre Stubbe <andrestubbe@proton.me>
2026-08-14 17:14:17 +0200

    Auto Update

--- examples/Benchmark/src/main/java/fasttts/benchmark/JMH_FastTTS.java
+++ examples/Benchmark/src/main/java/fasttts/benchmark/JMH_FastTTS.java
@@ -1,13 +1,25 @@
 package fasttts.benchmark;
+
 import fasttts.FastTTS;
+import fasttts.backends.windows.WindowsTTSBackend;
+import fasttts.core.FastTTSAudio;
 import org.openjdk.jmh.annotations.*;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Thread)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
-public class JMH_TTS {
+public class JMH_FastTTS {
     private FastTTS tts;
-    @Setup public void setup() { tts = FastTTS.createWindows(); }
-    @Benchmark public byte[] benchmarkSynthesize() { return tts.synthesize("FastJava SIMD Text to Speech"); }
+
+    @Setup
+    public void setup() throws Exception {
+        tts = new FastTTS();
+        tts.registerBackend(new WindowsTTSBackend());
+    }
+
+    @Benchmark
+    public FastTTSAudio benchmarkSynthesize() throws Exception {
+        return tts.speak("FastJava SIMD Text to Speech");
+    }
 }
